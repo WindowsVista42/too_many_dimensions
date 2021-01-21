d6a96b76e5825e65b866d2f5a302edd9220b25db
Sean Moulton <bananamoulton@gmail.com>
2021-01-21 14:42:42 -0600

    Multisampled rendering, alpha blending, render bundles

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ const FLOW_SHAPE_VERTICES: [FlowVertex; 4] = [
 
 const FLOW_SHAPE_INDICES: [u16; 6] = [0, 1, 2, 0, 2, 3];
 
-const NUM_FLOW: usize = 5_000;
+const NUM_FLOW: usize = 100_000;
 
 fn main() {
     let event_loop = EventLoop::new();
@@ -98,6 +98,7 @@ struct State {
     queue: wgpu::Queue,
     sc_desc: wgpu::SwapChainDescriptor,
     swap_chain: wgpu::SwapChain,
+    sample_count: u32,
 
     // UNIFORMS
     camera: Camera,
@@ -125,6 +126,10 @@ struct State {
     flow_indices_buffer: wgpu::Buffer,
     flow_work_group_count: u32,
     flow_num_indices: u32,
+
+    // BUILD AFTER SELF
+    render_bundle: Option<wgpu::RenderBundle>,
+    multisampled_framebuffer: Option<wgpu::TextureView>,
 
     // WINDOW
     size: winit::dpi::PhysicalSize<u32>,
@@ -170,13 +175,16 @@ impl State {
             format: wgpu::TextureFormat::Bgra8UnormSrgb,
             width: size.width,
             height: size.height,
-            present_mode: wgpu::PresentMode::Mailbox,
+            present_mode: wgpu::PresentMode::Fifo,
         };
 
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
+        let sample_count = 4;
+
         // SHADER LOADING
-        let vs_module = device.create_shader_module(wgpu::include_spirv!("../spirv/vertex.vert.spv"));
+        let vs_module =
+            device.create_shader_module(wgpu::include_spirv!("../spirv/vertex.vert.spv"));
         let fs_module =
             device.create_shader_module(wgpu::include_spirv!("../spirv/fragment.frag.spv"));
         let flow_cs_module =
@@ -188,6 +196,9 @@ impl State {
 
         // UNIFORMS
         let camera = Camera {
+            changed: false,
+            slow_spd: 1.5,
+            fast_spd_fac: 2.0,
             pos: glam::Vec2::zero(),
             scl: 1.0,
             asp: sc_desc.width as f32 / sc_desc.height as f32,
@@ -266,7 +277,7 @@ impl State {
                 index_format: wgpu::IndexFormat::Uint16,
                 vertex_buffers: &[Vertex::desc()],
             },
-            sample_count: 1,
+            sample_count,
             sample_mask: !0,
             alpha_to_coverage_enabled: false,
         });
@@ -372,7 +383,16 @@ impl State {
                 ..Default::default()
             }),
             primitive_topology: wgpu::PrimitiveTopology::TriangleList,
-            color_states: &[sc_desc.format.into()],
+            color_states: &[wgpu::ColorStateDescriptor {
+                format: sc_desc.format,
+                alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                color_blend: wgpu::BlendDescriptor {
+                    src_factor: wgpu::BlendFactor::SrcAlpha,
+                    dst_factor: wgpu::BlendFactor::OneMinusSrcAlpha,
+                    operation: wgpu::BlendOperation::Add,
+                },
+                write_mask: wgpu::ColorWrite::ALL,
+            }],
             depth_stencil_state: None,
             vertex_state: wgpu::VertexStateDescriptor {
                 index_format: wgpu::IndexFormat::Uint16,
@@ -389,7 +409,7 @@ impl State {
                     },
                 ],
             },
-            sample_count: 1,
+            sample_count,
             sample_mask: !0,
             alpha_to_coverage_enabled: false,
         });
@@ -413,10 +433,16 @@ impl State {
             (4 * NUM_FLOW) as usize
         ];
         for p in initial_flow_data.iter_mut() {
-            p.pos[0] = 2.0 * (rand::random::<f32>() - 0.5); //posx
-            p.pos[1] = 2.0 * (rand::random::<f32>() - 0.5); //posy
+            p.pos[0] = 12.0 * (rand::random::<f32>() - 0.5); //posx
+            p.pos[1] = 12.0 * (rand::random::<f32>() - 0.5); //posy
             p.vel[0] = 2.0 * (rand::random::<f32>() - 0.5) * 0.1; //velx
             p.vel[1] = 2.0 * (rand::random::<f32>() - 0.5) * 0.1; //vely
+            if p.pos[0] == 0.0 {
+                p.pos[0] = 0.01;
+            }
+            if p.pos[1] == 0.0 {
+                p.pos[1] = 0.01;
+            }
         }
 
         let mut flow_buffers = Vec::<wgpu::Buffer>::with_capacity(2);
@@ -458,7 +484,9 @@ impl State {
 
         let flow_work_group_count = ((NUM_FLOW as f32) / (64.0)).ceil() as u32;
 
-        Self {
+        let flow_num_indices = 8;
+
+        let mut s = Self {
             // INPUT
             input,
 
@@ -473,6 +501,7 @@ impl State {
             queue,
             sc_desc,
             swap_chain,
+            sample_count,
 
             // UNIFORMS
             camera,
@@ -499,12 +528,80 @@ impl State {
             flow_vertices_buffer,
             flow_indices_buffer,
             flow_work_group_count,
-            flow_num_indices: 8,
+            flow_num_indices,
+
+            render_bundle: None,
+            multisampled_framebuffer: None,
 
             // WINDOW
             size,
             frame_num: 0,
+        };
+
+        s.create_render_bundles();
+        s.create_multisampled_framebuffer();
+
+        s
+    }
+
+    fn create_multisampled_framebuffer(&mut self) {
+        if self.sample_count > 1 {
+            let multisampled_texture_extent = wgpu::Extent3d {
+                width: self.sc_desc.width,
+                height: self.sc_desc.height,
+                depth: 1,
+            };
+            let multisampled_frame_descriptor = &wgpu::TextureDescriptor {
+                size: multisampled_texture_extent,
+                mip_level_count: 1,
+                sample_count: self.sample_count,
+                dimension: wgpu::TextureDimension::D2,
+                format: self.sc_desc.format,
+                usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+                label: None,
+            };
+            self.multisampled_framebuffer = Some(
+                self.device
+                    .create_texture(multisampled_frame_descriptor)
+                    .create_view(&wgpu::TextureViewDescriptor::default()),
+            );
+        } else {
+            self.multisampled_framebuffer = None;
         }
+    }
+
+    fn create_render_bundles(&mut self) {
+        self.render_bundle = Some({
+            let mut rpass =
+                self.device
+                    .create_render_bundle_encoder(&wgpu::RenderBundleEncoderDescriptor {
+                        label: Some("FLOW RENDER BUNDLE"),
+                        color_formats: &[self.sc_desc.format],
+                        depth_stencil_format: None,
+                        sample_count: self.sample_count,
+                    });
+
+            // RENDER BACKGROUND
+            /*
+            rpass.set_pipeline(&self.render_pipeline);
+            rpass.set_bind_group(0, &self.view_uniform_bind_group, &[]);
+            rpass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            rpass.set_index_buffer(self.index_buffer.slice(..));
+            rpass.draw_indexed(0..self.num_indices, 0, 0..1);
+            */
+
+            // RENDER FLOW PARTICLES
+            rpass.set_pipeline(&self.flow_render_pipeline);
+            rpass.set_bind_group(0, &self.view_uniform_bind_group, &[]);
+            rpass.set_vertex_buffer(0, self.flow_buffers[0].slice(..));
+            rpass.set_vertex_buffer(1, self.flow_vertices_buffer.slice(..));
+            rpass.set_index_buffer(self.flow_indices_buffer.slice(..));
+            rpass.draw_indexed(0..self.flow_num_indices, 0, 0..NUM_FLOW as _);
+
+            rpass.finish(&wgpu::RenderBundleDescriptor {
+                label: Some("main"),
+            })
+        });
     }
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
@@ -512,6 +609,7 @@ impl State {
         self.sc_desc.width = new_size.width;
         self.sc_desc.height = new_size.height;
         self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+        self.create_multisampled_framebuffer();
 
         self.camera.asp = self.sc_desc.width as f32 / self.sc_desc.height as f32;
 
@@ -538,9 +636,11 @@ impl State {
     }
 
     fn render(&mut self) {
-        if !self.pause {
-            self.dbuf ^= 1;
-        }
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("RENDER ENCODER"),
+            });
 
         let frame = self
             .swap_chain
@@ -548,13 +648,13 @@ impl State {
             .expect("Timeout getting texture")
             .output;
 
-        let mut encoder = self
-            .device
-            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("RENDER ENCODER"),
-            });
-
         if !self.pause {
+            std::mem::swap(
+                &mut self.flow_buffers[0].slice(..),
+                &mut self.flow_buffers[1].slice(..),
+            ); // Swap buffers for rendering
+            self.dbuf ^= 1; // Swap idx for compute
+
             let mut cpass = encoder.begin_compute_pass();
             cpass.set_pipeline(&self.flow_compute_pipeline);
             cpass.set_bind_group(0, &self.flow_bind_groups[self.dbuf], &[]);
@@ -562,37 +662,35 @@ impl State {
         }
 
         {
-            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+            let ops = wgpu::Operations {
+                load: wgpu::LoadOp::Clear(wgpu::Color {
+                    r: 0.005,
+                    g: 0.005,
+                    b: 0.005,
+                    a: 1.0,
+                }),
+                store: true,
+            };
+
+            let rpass_color_attachment = match self.multisampled_framebuffer {
+                Some(_) => wgpu::RenderPassColorAttachmentDescriptor {
+                    attachment: &self.multisampled_framebuffer.as_ref().unwrap(),
+                    resolve_target: Some(&frame.view),
+                    ops,
+                },
+                None => wgpu::RenderPassColorAttachmentDescriptor {
                     attachment: &frame.view,
                     resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 1.0,
-                        }),
-                        store: true,
-                    },
-                }],
+                    ops,
+                },
+            };
+
+            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                color_attachments: &[rpass_color_attachment],
                 depth_stencil_attachment: None,
             });
 
-            // RENDER BACKGROUND
-            rpass.set_pipeline(&self.render_pipeline);
-            rpass.set_bind_group(0, &self.view_uniform_bind_group, &[]);
-            rpass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            rpass.set_index_buffer(self.index_buffer.slice(..));
-            rpass.draw_indexed(0..self.num_indices, 0, 0..1);
-
-            // RENDER FLOW PARTICLES
-            rpass.set_pipeline(&self.flow_render_pipeline);
-            rpass.set_bind_group(0, &self.view_uniform_bind_group, &[]);
-            rpass.set_vertex_buffer(0, self.flow_buffers[self.dbuf ^ 1].slice(..));
-            rpass.set_vertex_buffer(1, self.flow_vertices_buffer.slice(..));
-            rpass.set_index_buffer(self.flow_indices_buffer.slice(..));
-            rpass.draw_indexed(0..self.flow_num_indices, 0, 0..NUM_FLOW as _);
+            rpass.execute_bundles(&mut self.render_bundle.iter());
         }
 
         self.frame_num += 1;
