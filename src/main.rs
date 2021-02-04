dd0c166b5ec8818cf47f6610b03aacd5089215c4
Sean Moulton <bananamoulton@gmail.com>
2021-02-03 18:30:47 -0600

    Simple debug messages

--- src/main.rs
+++ src/main.rs
@@ -22,14 +22,25 @@ fn toggle_fullscreen(state: &mut State, window: &Window) {
 }
 
 fn main() {
+    println!("{:>5} ms | Game init", 0);
+    let p_st = std::time::Instant::now();
     let event_loop = EventLoop::new();
+    println!("{:>5} ms | Event Loop created", p_st.elapsed().as_millis());
 
     let window = WindowBuilder::new()
         .with_title("Too Many Dimensions")
         .build(&event_loop)
         .unwrap();
+    println!("{:>5} ms | Window created", p_st.elapsed().as_millis());
 
+    println!("{:>5} ms | Game state started", p_st.elapsed().as_millis());
+    let now = std::time::Instant::now();
     let mut state = block_on(State::new(&window, 8));
+    println!(
+        "{:>5} ms | Game state finished ({} ms)",
+        p_st.elapsed().as_millis(),
+        now.elapsed().as_millis()
+    );
 
     event_loop.run(move |event, _, control_flow| {
         state.update(&event);
