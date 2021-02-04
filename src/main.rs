a537d1ccf5ddbdbf6581d03c3cd8f1318389544e
Sean Moulton <bananamoulton@gmail.com>
2021-02-03 20:22:55 -0600

    Compressed logs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![feature(const_ptr_offset_from, const_maybe_uninit_as_ptr, const_raw_ptr_deref)]
 
+#[macro_use]
+extern crate log;
+
 mod flow;
 mod state;
 mod view;
@@ -21,31 +24,54 @@ fn toggle_fullscreen(state: &mut State, window: &Window) {
     }
 }
 
-fn main() {
-    println!("{:>5} ms | Game init", 0);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let p_st = std::time::Instant::now();
+
+    let local_now = chrono::Local::now();
+    let name_format = local_now.format("%Y-%m-%d-%H-%M-%S");
+
+    flexi_logger::Logger::with(
+        flexi_logger::LogSpecification::default(flexi_logger::LevelFilter::max()).build(),
+    )
+    .log_to_file()
+    .directory("log")
+    .suffix("log")
+    .print_message()
+    .rotate(
+        flexi_logger::Criterion::Size(u64::MAX),
+        flexi_logger::Naming::Numbers,
+        flexi_logger::Cleanup::KeepCompressedFiles(usize::MAX),
+    )
+    .create_symlink("current_run")
+    .start()?;
+
+    info!("{}", name_format);
+    info!("Program Start");
+
+    let mut exited = false;
+
     let event_loop = EventLoop::new();
-    println!("{:>5} ms | Event Loop created", p_st.elapsed().as_millis());
+    info!("Event Loop Created");
 
     let window = WindowBuilder::new()
         .with_title("Too Many Dimensions")
         .build(&event_loop)
         .unwrap();
-    println!("{:>5} ms | Window created", p_st.elapsed().as_millis());
+    info!("Window Created");
 
-    println!("{:>5} ms | Game state started", p_st.elapsed().as_millis());
     let now = std::time::Instant::now();
+    info!("State Start");
     let mut state = block_on(State::new(&window, 8));
-    println!(
-        "{:>5} ms | Game state finished ({} ms)",
-        p_st.elapsed().as_millis(),
-        now.elapsed().as_millis()
-    );
+    info!("State End ({} ms)", now.elapsed().as_millis());
 
     event_loop.run(move |event, _, control_flow| {
         state.update(&event);
         if state.quit {
             *control_flow = ControlFlow::Exit;
+            if !exited {
+                info!("Program Close ({} s)", p_st.elapsed().as_secs());
+                exited = true;
+            }
             return;
         }
 
